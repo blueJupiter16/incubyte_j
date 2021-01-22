5e1ea53b9f30d24193bb66ad70909e40fda1229a
Junaid Tinwala <junaid.tinwala.96@gmail.com>
2021-01-22 21:48:25 +0530

    modified SQL & initial code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import numpy as np
+import pandas as pd
+record_details = pd.read_csv('/Users/junaidtinwala/Work/Projects/incubyte/inputs/record_details.csv')
+batch_df = pd.read_csv('/Users/junaidtinwala/Work/Projects/incubyte/inputs/batch_data/batch1_20210122',sep='|')
 
-
+print(batch_df)
