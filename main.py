e51ef8a12a1169559ad59b6d8fe8cff15b5d5ea2
Junaid Tinwala <junaid.tinwala.96@gmail.com>
2021-01-22 21:55:43 +0530

    completed inputs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+
+#Read files/tables - create initial databases
 record_details = pd.read_csv('/Users/junaidtinwala/Work/Projects/incubyte/inputs/record_details.csv')
 batch_df = pd.read_csv('/Users/junaidtinwala/Work/Projects/incubyte/inputs/batch_data/batch1_20210122',sep='|')
+reference_df = pd.read_csv('/Users/junaidtinwala/Work/Projects/incubyte/inputs/reference_table.csv')
 
-print(batch_df)
+print(reference_df)
